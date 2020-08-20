9c70eba36b9de3b1d68becf064653c5647801806
youngchunse <young.chun.se@gmail.com>
2020-08-20 14:34:10 -0400

    test

--- main.tf
+++ main.tf
@@ -1,6 +1,16 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket               = "terraform-state-dev-application"
+    dynamodb_table       = "terraform-state-dev-application-lock"
+    key                  = "dev/us-east-1/route"
+    region               = "us-east-1"
+  }
+}
+
 module "route" {
   source = "./route53"
 }
